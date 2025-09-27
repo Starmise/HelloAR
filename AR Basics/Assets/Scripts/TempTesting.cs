ba2f66bb6d742f87e5b4dfb19d5e148499f3df7e
Luis R <78448904+Starmise@users.noreply.github.com>
2025-09-27 08:58:20 -0600

    Imported Models
    Imported 4 new models from sketchfab to use in the infinite runner game.:
    - Joker's mask from Persona 5 to be used as points item.
    - Persona 4 evil hablerie for the enemies
    - Persona 4 tvs to be used as an obstacle
    - Heart Marble to be used as a health item

---
+++ AR Basics/Assets/Scripts/TempTesting.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class TempTesting : MonoBehaviour
+{
+    public void targetDetected()
+    {
+        Debug.Log("Hostia que sí funciona tú");
+    }
+
+    public void targetLost()
+    {
+        Debug.Log("Hostia no hay señal");
+    }
+}
