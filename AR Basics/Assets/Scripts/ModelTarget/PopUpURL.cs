d4fc3bce24424c968f07f8c187fb6a8e48c06b6e
Luis R <78448904+Starmise@users.noreply.github.com>
2025-11-11 20:52:06 -0600

    PopUp fix

--- AR Basics/Assets/Scripts/ModelTarget/PopUpURL.cs
+++ AR Basics/Assets/Scripts/ModelTarget/PopUpURL.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using Vuforia;
 
 public class PopUpURL : MonoBehaviour
 {
@@ -6,7 +8,11 @@ public class PopUpURL : MonoBehaviour
     public float delay = 30f;
     public string url;
 
+    [Header("Vuforia Target")]
+    public ObserverBehaviour modelTarget;
+
     private bool popupActive = false;
+    private bool timerStarted = false;
 
     private void Start()
     {
@@ -15,10 +21,21 @@ public class PopUpURL : MonoBehaviour
             popupPanel.SetActive(false);
         }
 
-        StartCoroutine(ShowPopupAfterDelay());
+        if (modelTarget != null)
+        { modelTarget.OnTargetStatusChanged += OnTargetStatusChanged; }
+
     }
 
-    private System.Collections.IEnumerator ShowPopupAfterDelay()
+    private void OnTargetStatusChanged(ObserverBehaviour behaviour, TargetStatus status)
+    {
+        if (!timerStarted && status.Status == Status.TRACKED || status.Status == Status.EXTENDED_TRACKED)
+        {
+            timerStarted = true;
+            StartCoroutine(ShowPopupAfterDelay());
+        }
+    }
+
+    private IEnumerator ShowPopupAfterDelay()
     {
         yield return new WaitForSeconds(delay);
         popupActive = true;
@@ -52,6 +69,14 @@ public class PopUpURL : MonoBehaviour
             yield return new WaitForSeconds(0.3f);
             cg.alpha = 1f;
             yield return new WaitForSeconds(0.3f);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (modelTarget != null)
+        {
+            modelTarget.OnTargetStatusChanged -= OnTargetStatusChanged;
         }
     }
 }
